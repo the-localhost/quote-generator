7ad89992a5a9c828f68afe98bac95a6eef178f93
Gagan Agrawal <44469150+the-localhost@users.noreply.github.com>
2021-05-25 13:28:51 +0530

    changed the proxy

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ const removeLoadingSpinner = () => {
 // getting the quote from the API
 async function getQuote(event, params = 0) {
     showLoadingSpinner();
-    const proxy = 'http://cors-anywhere.herokuapp.com/';
+    //const proxy = 'http://cors-anywhere.herokuapp.com/';
+    const proxy = 'https://peaceful-plains-68186.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxy + apiUrl);
